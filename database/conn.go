48732322f992137ae25b9ff7039f150214d82962
luckyLiu <mrliuxiansen8023@gmail.com>
2020-11-12 16:16:25 +0800

    database 增加gormV2支持

--- database/conn.go
+++ database/conn.go
@@ -7,16 +7,35 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/pkg/errors"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/driver/sqlite"
+	gormV2 "gorm.io/gorm"
 )
 
 // NewConn returns database conn.
-func NewConn(databaseURL string) (*gorm.DB, error) {
+func NewConn(dns string) (*gorm.DB, error) {
 	dialect := "mysql"
-	if strings.HasSuffix(databaseURL, ".db") {
+	if strings.HasSuffix(dns, ".db") {
 		dialect = "sqlite3"
 	}
 
-	conn, err := gorm.Open(dialect, databaseURL)
+	conn, err := gorm.Open(dialect, dns)
+	if err != nil {
+		return nil, errors.Wrap(err, "database: open connection")
+	}
+
+	return conn, nil
+}
+
+// NewConnV2 returns database conn by gormV2.
+func NewConnV2(dns string, config *gormV2.Config) (*gormV2.DB, error) {
+	dialect := mysql.Open(dns)
+	if strings.HasSuffix(dns, ".db") {
+		dialect = sqlite.Open(dns)
+	}
+
+	conn, err := gormV2.Open(dialect, config)
 	if err != nil {
 		return nil, errors.Wrap(err, "database: open connection")
 	}
--- go.mod
+++ go.mod
@@ -5,4 +5,7 @@ go 1.15
 require (
 	github.com/jinzhu/gorm v1.9.16
 	github.com/pkg/errors v0.9.1
+	gorm.io/driver/mysql v1.0.3
+	gorm.io/driver/sqlite v1.1.3
+	gorm.io/gorm v1.20.6
 )
